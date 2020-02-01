684b6155f9367193d35899df49dd0a30fa778264
Tolulope <tolesy2003@outlook.com>
2020-02-01 23:48:12 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -6,18 +6,41 @@ class User:
         self.email = email
         self.age =age
         
-def greeting(self):
-    return ('My name is {self.name} and I am {self.age}')
-        
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age}'
+
+    def has_birthday(self):
+        self.age += 1
+
+ # Extending User with customer class
+class Customer(User):
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age =age
+        self.balance = 0
+    def set_balance(self, balance):
+        self.balance = balance
+     # Overide greeting() method in class User
+    
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age} and I owe a balance of {self.balance}'
 # Init user object
 
 Tolu = User('Tolu Ademilua', 'tolu@gmail.com', 33)
-Precious = User('Precious Ademilua', 'precious@gmail.com', 33)
+Precious = User('Precious Ademilua', 'precious@gmail.com', 32)
 
 # Edit Property
 Tolu.age = 34
+
+Tolu.has_birthday()
+ # Init Customer
+James = Customer('James brat', 'james@gmail.com', 50)
+James.set_balance(500)
+print(James.name)
 #print(Tolu.age)
-#print(Precious.email)
+print(Precious.email)
+print(James.greeting())
 
 # call methods
 
